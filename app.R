743f2cf3cf5df708eae38e3c97671d2c59c4608a
Owen F. O'Connor <148789982+ThatObiGuy@users.noreply.github.com>
2024-12-24 14:16:41 +0000

    Update app.R
    Did some work on UI, committing in this state before starting work on server side incase we need to roll back.

--- app.R
+++ app.R
@@ -1,36 +1,50 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# Paycheck - 2 - paycheck
+# Owen F. O'Connor
 
 library(shiny)
+library(shinythemes) # Adding themeing package, quick way to make app more appealing with minimal effort
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# Define UI for application
+ui <- fluidPage(theme = shinytheme("superhero"), # Implementation of shinythemes library called above - I like the superhero theme, found @ https://rstudio.github.io/shinythemes/
+    navbarPage(
+      title = "Paycheck2Paycheck", # Text displayed in the top left corner
+      
+      tabPanel("Forecasting", # This is the title of this panel - the spending forecast happens here
+               
+               sidebarPanel( # Our "input" panel
+                 width = 3, # spacing, allowing for the 3 side-by-side segments
+                 tags$h3("Input:"),
+                 textInput("txt1", "Given Name:", ""),
+                 textInput("txt2", "Surname:", ""),
+                 
+               ), # sidebarPanel - input ends
+               
+               mainPanel(
+                 width = 6, # spacing, allowing for the 3 side-by-side segments
+                 h1("This Paycheck:"),
+                 verbatimTextOutput("txtout"),
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+               ), # mainPanel ends
+               
+               sidebarPanel( # Our "image" panelS
+                 width = 3, # spacing, allowing for the 3 side-by-side segments
+                 h4("How we're feeling:"),
+                 img(src="https://dudewipes.com/cdn/shop/articles/gigachad.jpg?v=1667928905&width=1024",
+                     height = 150,
+                     width = 300), # Placeholder image for now.
+                 
+               ), # sidebarPanel - image  ends
+               
+      ), # tabPanel - Forecasting  ends
+      
+      tabPanel("About", "This panel will be developed in time"),
+  
+    ) # navbarPage ends
+    
+  ) # fluidPage ends
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
