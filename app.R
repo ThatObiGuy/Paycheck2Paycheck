d0139107b991cd308a262acb30663c906c45a10d
Owen F. O'Connor <148789982+ThatObiGuy@users.noreply.github.com>
2025-02-10 11:41:20 +0000

    Update app.R 10/02/2025
    > Real demo video
    > More comments
    > More reactions
    > Earn/Spend buttons instead of single commit.
    > Upload/Download functionality

--- app.R
+++ app.R
@@ -7,24 +7,24 @@ library(shinythemes) # Adding themeing package, quick way to make app more appea
 # Define UI for application
 ui <- fluidPage(theme = shinytheme("superhero"), # Implementation of shinythemes library called above - I like the superhero theme, found @ https://rstudio.github.io/shinythemes/
   
-  navbarPage(
+  navbarPage( # Allows us to use navigation bar at the top of the page, and structure in tabs
     title = "Paycheck2Paycheck", # Text displayed in the top left corner
     
-    tabPanel("Forecasting", # This is the title of this panel - the spending forecast happens here
+    tabPanel("Forecasting", # This is the title of this panel, displayed in our navbar - the spending forecast happens here
              
-             sidebarPanel( # Our "input" panel
+             sidebarPanel( # Our "input" panel within the forecasting tab
                width = 3, # spacing, allowing for the 3 side-by-side segments
                
-               tags$h3("Parameters:"), # Here we'll prompt for paycheck amount and days till next, these will help us construct the graph
+               tags$h3("Parameters:"), # Here we'll prompt for paycheck amount and days till next, these will help us construct the graph. We can use tags to access HTML from shiny
                numericInput("paycheck", "Paycheck amount", value = 0, min = 0, max = 5000), # Must be non-negative
                numericInput("rent", "rent contribution", value = 0, min = 0, max = 5000), # Must be non-negative
                numericInput("days", "Days till next paycheck", value = 0, min = 0, max = 365), # Also must be non-negative
-               actionButton("buildGraphButton", "Build Graph", class = "btn-lg btn-success"), # Button 'locks-in' entered values
+               actionButton("buildGraphButton", "Build Graph", class = "btn-lg btn-success"), # Button 'locks-in' entered values. classes lg makes it large, and success makes it green
                
-               tags$h3("Daily income/expenditure:"), # Here we'll take in data on spending/earning that will populate the graph
+               tags$h3("Daily aggregate:"), # Here we'll take in data on spending/earning that will populate the graph
                numericInput("moneyMove", "Money Move", value = 0, min = -1000, max = 1000), # Money can move in or out of ones account.
-               actionButton("moneyMoveButton", "Commit", class = "btn-lg btn-success"), # Button 'locks-in' entered value for a given day
-               
+               actionButton("moneyEarnButton", "EARN", class = "btn-lg btn-success"), # Buttons 'locks-in' entered value for a given day
+               actionButton("moneySpendButton", "SPEND", class = "btn-lg btn-danger"), # A green EARN and red SPEND button make app more intuative
              ), # sidebarPanel - input ends
              
              mainPanel(
@@ -32,18 +32,38 @@ ui <- fluidPage(theme = shinytheme("superhero"), # Implementation of shinythemes
                h1("This Paycheck:"),
                
                plotOutput("spendPlot"), # Displays plot generated on server side.
-               
              ), # mainPanel ends
              
-             sidebarPanel( # Our "image" panel
+             sidebarPanel( # Our "image" panel - Here we provide feedback through images
                width = 3, # spacing, allowing for the 3 side-by-side segments
                
                h4("How we're feeling:"),
-               uiOutput("feelingImage"), # Using uiOutput allows image to react to lm's prediction
+               uiOutput("feelingImage"), # Using uiOutput allows image to react to lm's slope.
                
                h4("Advice:"),
                uiOutput("adviceText")
              ), # sidebarPanel - image  ends
+             
+             sidebarPanel( # Our "Upload/Download" panel - Here we add functionality to save and resume previous progress
+               selectInput("menu", "Upload/Download session progress:", # Drop-down style menu makes page appear "less-busy"
+                           choices = c("Select an option:", "Upload", "Download")),
+               conditionalPanel( # Conditional panels appear when their 'choice' from above is selected
+                 condition = "input.menu == 'Select an option:'",
+               ),
+               conditionalPanel(
+                 condition = "input.menu == 'Upload'",
+                 h4("Resume progress:"),
+                 fileInput("upload", "Choose .rda File", # Use of .rda file allows for me to use less code, and makes sense to me, an r enjoyer.
+                           multiple = FALSE, # Only makes sense to upload one file, app can only handle 1 session at a given time.
+                           accept = c(".rda")), # Makes sure extension of file is .rda
+               ), # fileInput gives us a button to chose file, upload progress from different session
+               conditionalPanel(
+                 condition = "input.menu == 'Download'",
+                 h4("Save for later:"),
+                 downloadButton("downloadData", "Download"), # title name for us to refer to from server side and label for button
+               ), # fileInput gives us a button for download if user wants to come back to it later
+
+             ), # sidebarPanel - download/upload  ends
              
     ), # tabPanel - Forecasting  ends
     
@@ -52,12 +72,15 @@ ui <- fluidPage(theme = shinytheme("superhero"), # Implementation of shinythemes
             h2("How to Use the App"),
             p("1. Enter your paycheck amount and the number of days until your next paycheck."),
             p("2. Click 'Build Graph'."),
-            p("3. Input your daily aggregate income/expenditure"),
+            p("3. Input your daily aggregate earnings/spending"),
             p("4. Use the advice and visual feedback to make more informed financial decisions ;)"),
-            br(),
-            tags$iframe(width="560", height="315", src="https://www.youtube.com/embed/yInCE7z2aM4?si=I2e91TrUBemGhsNw",
-                        frameborder="0", allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
-                        allowfullscreen=NA,) # Placeholder video for now, need to put video together on how-to
+            hr(),
+            p("You can watch the video below as an example and guide."),
+            div(style = "text-align: center;", 
+                tags$iframe(width="560", height="315",
+                            src="https://www.youtube.com/embed/OErMB4WYkjE?si=0hzi8FV5ioTW2dlI",
+                            frameborder="0", allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
+                            allowfullscreen=NA)) # A quick video showing how app is to be used.
             
     ), # tabPanel - How to use ends
     
@@ -100,6 +123,18 @@ server <- function(input, output) {
                               imageURL = "https://raw.githubusercontent.com/ThatObiGuy/Paycheck2Paycheck/refs/heads/main/ImagesResized/Waiting.png",
                               adviceText = "waiting on more data before providing advice")
   
+  observeEvent(input$upload, { # observeEvents looks for user to upload file, at which point it can update resume previous session
+    req(input$upload)
+    load(input$upload$datapath)
+    user_data$days <- user_data_list$days # Assigns the values from the lists (uploaded) to this session's values
+    user_data$paycheck <- user_data_list$paycheck
+    user_data$rent <- user_data_list$rent
+    user_data$moneyMoves <- unlist(user_data_list$moneyMoves)
+    user_data$pressCounter <- user_data_list$pressCounter
+    user_data$imageURL <- user_data_list$imageURL
+    user_data$adviceText <- user_data_list$adviceText
+  })
+  
   observeEvent(input$buildGraphButton, { # observeEvents looks for 'build graph' button presses, at which point it can update the stored values
     user_data$days <- input$days
     user_data$paycheck <- input$paycheck
@@ -110,9 +145,14 @@ server <- function(input, output) {
     user_data$adviceText <- "waiting on more data before providing advice"
   })
   
-  observeEvent(input$moneyMoveButton, { # Appends moneyMoves vector with new values, each ideally for consecutive days
+  observeEvent(input$moneyEarnButton, { # Appends moneyMoves vector with new values, each ideally for consecutive days
     user_data$pressCounter <- user_data$pressCounter + 1
     user_data$moneyMoves[user_data$pressCounter] <- input$moneyMove
+  })
+  
+  observeEvent(input$moneySpendButton, { # Appends moneyMoves vector with new values, each ideally for consecutive days
+    user_data$pressCounter <- user_data$pressCounter + 1
+    user_data$moneyMoves[user_data$pressCounter] <- (input$moneyMove * -1)
   })
   
   output$spendPlot <- renderPlot({
@@ -160,7 +200,10 @@ server <- function(input, output) {
       
       
       # Update image based on the slope of the linear model
-      if ((user_data$paycheck - user_data$rent) + user_data$days*coef(fit)[2] > 0) { # we expect to have some money at when we receive next paycheck
+      if ((user_data$paycheck - user_data$rent) + user_data$days*coef(fit)[2] > (user_data$paycheck - user_data$rent)){
+        user_data$imageURL <- "https://raw.githubusercontent.com/ThatObiGuy/Paycheck2Paycheck/refs/heads/main/ImagesResized/Swimming.png"
+        user_data$adviceText <- "You don't need this app"
+      } else if ((user_data$paycheck - user_data$rent) + user_data$days*coef(fit)[2] > 0) { # we expect to have some money at when we receive next paycheck
         user_data$imageURL <- "https://raw.githubusercontent.com/ThatObiGuy/Paycheck2Paycheck/refs/heads/main/ImagesResized/gigachad.png"
         user_data$adviceText <- "Keep it up, You've got MONEY!"
       } else if ((user_data$paycheck - user_data$rent) + user_data$days*coef(fit)[2] == 0) { # we expect to have no debt when we receive next paycheck
@@ -185,6 +228,15 @@ server <- function(input, output) {
     p(user_data$adviceText)
   })
   
+  output$downloadData <- downloadHandler(
+    filename = function() { # Filename is based on time of save
+      paste("Paycheck2Paycheck-Save-", Sys.Date(), ".rda", sep="")
+    },
+    content = function(file) { # .rda structure allows us to be very efficient
+      user_data_list <- reactiveValuesToList(user_data)
+      save(user_data_list, file = file)
+    }
+  )
 }
 
 # Run the application 
